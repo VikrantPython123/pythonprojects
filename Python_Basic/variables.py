1b11c76e64ceb6bb64388c0925ea732ce991bcb8
VikrantPython <vikrant200110@gmail.com>
2023-06-28 23:50:59 +0530

    third

--- Python_Basic/variables.py
+++ Python_Basic/variables.py
@@ -13,6 +13,17 @@ print(string_1)
 num_float_1 = 23.0
 print(type(num_float_1))   # <class 'float'>
 
+#### using functions to calculate the pythagoras value
+
+def Pythagoras(a,b):
+    c = a*a + b*b
+    d = "{:.2f}".format(math.sqrt(c))
+    return d
+print("Enter Base Value:")
+a = float(input())
+print("Enter Perpendicular Value:")
+b = float(input())
+print(Pythagoras(a,b))
 ## now if we want floating value to increase the decimal point we will use .2f or .3f for decimal points
 """So %. 2f means to round up to two decimal places. 
 You can play around with the code to see what happens as you change the number in the formatter."""
@@ -34,17 +45,7 @@ d = "{:.2f}".format(math.sqrt(c))
 print(d)
 
 
-#### using functions to calculate the pythagoras value
 
-def Pythagoras(a,b):
-    c = a*a + b*b
-    d = "{:.2f}".format(math.sqrt(c))
-    return d
-print("Enter Base Value:")
-a = float(input())
-print("Enter Perpendicular Value:")
-b = float(input())
-print(Pythagoras(a,b))
 
 
 ### program making use of init functions
